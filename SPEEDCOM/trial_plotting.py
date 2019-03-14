7cb9ea09f3cbbd348f9dcf9f24e1685bda62e2bb
Ryan Beck <ryan@D-172-25-128-111.dhcp4.washington.edu>
2019-03-14 16:02:57 -0700

    Have trial plotting function started as well as trial data

---
+++ SPEEDCOM/trial_plotting.py
@@ -0,0 +1,14 @@
+#Python file to do the trial plotting.  Should be discarded as is unnecessary
+#for master.
+
+import pandas as pd
+import numpy as np
+
+data_file = 'models/trial_abs_ems_data.tsv'
+
+def visualize(data):
+    """
+    
+    """
+    return
+

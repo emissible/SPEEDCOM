f83eb1312feedc4d2018023188d813895e80ad47
Ryan Beck <ryan@D-172-25-128-111.dhcp4.washington.edu>
2019-03-17 22:52:24 -0700

    resolved error with utilities importing NNModels

--- speedcom/tests/test_speedcom.py
+++ speedcom/tests/test_speedcom.py
@@ -1,12 +1,17 @@
+"""
+Unit tests for the speedcom.py file
+"""
 
+import unittest
+import speedcom.tests.context as context
 
-class initiate_molecules(unittest.TestCase):
+class initiate_molecules(context.speedcom.TestCase):
     def test_proper_size(self):
         """
         Ensure the appropriate number of molecule objects are returned
         upon initialization.
         """
-        assert len(context.speedcom.initiate_molecules(testing_dir)) == 3,\
+        assert len(speedcom.initiate_molecules(testing_dir)) == 3,\
             "initiate_molecules unable to find all molecules"
         return
 

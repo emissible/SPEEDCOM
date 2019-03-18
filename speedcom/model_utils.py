da4247e180da2257174e0283fd39c5cbf6bcb849
yliu92uw <yliu92@uw.edu>
2019-03-18 16:13:06 -0700

    added doc string for model_utils.py

--- speedcom/model_utils.py
+++ speedcom/model_utils.py
@@ -28,7 +28,8 @@ class ModelUtils:
     
   
     @staticmethod
-    def plot_model_error(x_train, x_test, y_train, y_test, model, save_fig_fname, label):
+    def plot_model_error(x_train, x_test, y_train, y_test, model, \
+            save_fig_fname, label):
         """
         input model and actual data, plot train, test predicted data and error
         """
@@ -37,15 +38,17 @@ class ModelUtils:
         y_test = y_test.reshape(-1,1)
         plt.figure(figsize=(8, 10), dpi=100)
         plt.subplot(211)
-        plt.scatter(y_train, model.predict(X_train), color = 'r', label = 'train')
-        plt.scatter(y_test, model.predict(X_test), color = 'blue', label = 'test')
+        plt.scatter(y_train, model.predict(X_train), color='r', label='train')
+        plt.scatter(y_test, model.predict(X_test), color='blue', label='test')
         plt.xlabel('Actual %s' % label, fontsize=12)
         plt.ylabel('Predicted %s' %label, fontsize=12)
         plt.legend(loc='upper center')
 
         plt.subplot(212)
-        plt.scatter(y_train, model.predict(X_train)-y_train, color = 'r', label = 'train', marker= 'x')
-        plt.scatter(y_test, model.predict(X_test)-y_test, color = 'blue', label = 'test', marker = 'x')
+        plt.scatter(y_train, model.predict(X_train)-y_train, color = 'r', \
+                label = 'train', marker= 'x')
+        plt.scatter(y_test, model.predict(X_test)-y_test, color = 'blue', \
+                label = 'test', marker = 'x')
         plt.axhline(0, ls='--')
         plt.xlabel('Actual %s' % label, fontsize=12)
         plt.ylabel('Prediction error %s' % label, fontsize=12)
@@ -59,9 +62,13 @@ class ModelUtils:
     @staticmethod
     def combine_columns(nd_arrays):
         """
-        combine nd array as columns
-        nd_arrays: tuple of nd arrays of same dimension
-        return: np.array
+        column combine multiple arrays
+        Args:
+        -----
+        nd_arrays: list of array of same length
+        return: 
+        -----
+        np.array
         """
         return np.column_stack((nd_arrays))
     
@@ -69,13 +76,17 @@ class ModelUtils:
     def get_y_category(y_actual,min_edge, bin_width):
         """
         get assing Y into classes based on y actual value
+        Args:
+        -----
+        y_actual:  np.array
+        min_edge:  int/float
+        bin_width: int/foat
 
-        y_actual -- np.array
-        min_edge -- int/float
-        bin_width --int/foat
-
-        return Y_category -- np.array:  class variable of Y
+        return:
+        Y_category: np.array
         """
+        assert y_actual.max() > min_edge, 'min_edge \
+                has to be smaller than maximum y_actual'
         Y_category = (y_actual - min_edge) // bin_width
         return Y_category
     
@@ -84,10 +95,13 @@ class ModelUtils:
     def get_class_count(Y_category):
         """
         get counts of each class and return as dictionary
-
+        Args:
+        -----
         Y_category -- np.array: categorical y
-        return cls_counts -- dict: {class(key): counts(value)}
-
+        
+        return:
+        -----
+        cls_counts -- dict: {class(key): counts(value)}
         """
         cls, counts = np.unique(Y_category, return_counts = True)
         cls_counts = dict(zip(cls, counts))
@@ -99,13 +113,16 @@ class ModelUtils:
     def subsampling(dataset, class_column_index, class_max_count, class_dict):
         """
         subsample each class to make sure every class has closer counts
-
+        Args:
+        -----
         dataset -- nd.array: includes x column, y(category) etc
         class_column_index -- int: index of the category data in dataset
         class_max_count -- int: maximum number of counts allowed in each class 
         class_dict -- dict: {class: count}
-
-        return subsampled data -- nd array
+       
+        return:
+        -----
+        subsampled data -- np.array
         """
         out = []
         for row in dataset:
@@ -119,6 +136,14 @@ class ModelUtils:
     def onehot_encode_y(y, num_class):
         """
         one-hot encoding of categorical y
+        Args:
+        -----
+        y: np.array
+        num_class: int
+
+        return:
+        np.array
+        -----
         """
         one_hot = np.zeros((y.shape[0],num_class),dtype=np.int8)
         for index, cls in enumerate(y):
@@ -129,14 +154,32 @@ class ModelUtils:
     def coeff_determination(y_true, y_pred):
         """
         calculates R square from y actual and y predict
+        Args:
+        -----
+        y_true: np.array
+        y_pred: np.array
+
+        return:
+        -----
+        R_square: float
         """
+        assert y_true.shape[0] = y_pred.shape[0]
         from keras import backend as K
         SS_res =  K.sum(K.square( y_true-y_pred ))
-        SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) )
-        return ( 1 - SS_res/(SS_tot + K.epsilon()) )
+        SS_tot = K.sum(K.square( y_true - K.mean(y_true)))
+        return ( 1 - SS_res/(SS_tot + K.epsilon()))
     
     @staticmethod
     def get_lr_metric(optimizer):
+        """
+        get learning rate from optimizer
+        Args:
+        -----
+
+        return:
+        -----
+        lr (float)
+        """
         def lr(y_true, y_pred):
             return optimizer.lr
         return lr
